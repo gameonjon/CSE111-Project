55db1afe9436e526f5e808cac4cef0ce72ab5581
gameonjon <39804998+gameonjon@users.noreply.github.com>
2020-11-12 17:15:57 -0800

    Update VideoGame.py

--- VideoGame.py
+++ VideoGame.py
@@ -1,5 +1,6 @@
 import sqlite3
 from sqlite3 import Error
+import time
 
 def openConnection(_dbFile):
     print("++++++++++++++++++++++++++++++++++")
@@ -43,12 +44,12 @@ def createTables(_conn):
                     g_genre varchar(15) NOT NULL,
                     g_exclusive boolean,
                     g_pubkey decimal(12,0),
-                    g_devkey decimal(12,0),
+                    g_devkey decimal(12,0)
                     )"""
         _conn.execute(sql)
 
         sql = """CREATE TABLE Platform (
-                    pf_system varchar(12) NOT NULL
+                    pf_system varchar(12) NOT NULL,
                     pf_exclusive boolean)"""
         _conn.execute(sql)
 
@@ -110,8 +111,134 @@ def dropTables(_conn):
     print("++++++++++++++++++++++++++++++++++")
 
 
-def populateTables(_conn):
+def populateTable_Games(_conn):
     print("++++++++++++++++++++++++++++++++++")
     print("Populate table")
 
+    try:
+        games = [               #(title, year, genre, exclusive, pubkey, devkey)
+
+            #Platform:
+            # ps4, xbox1, xbox360, MSW, Mac OS, Linux
+            ("Rise of the Tomb Raider", '2016-02-09', "action-adventure", 0, 10001, 20001),
+            #Platform: ps4, xbox1, MSW
+            ("Star Wars: Jedi Fallen Order",'2019-11-15', "action-adventure", 0, 10004, 20005),
+            #Platform: ps4, xbox1, MSW
+            ("Star Wars: BattleFront 2", '2017-11-17', "shooter", 0, 10004, 20006),
+            #Platform: ps4, MSW
+            ("Death Stranding", '2019-11-08', "action", 0, 10006, 20009),
+            #Platform: ps4, nintendo switch, xbox 1, msw
+            ("Overwatch", "2016-05-24", "shooter", 0, 10007, 20010),
+            #Platform: ps4, xbox1, msw, nintendo switch
+            ("Bioshock: The Collection", '2016-09-13', "shooter", 0, 10008, 20012),
+            #Platform: ps4, xbox1, msw, linux, classic Mac os
+            ("Dying Light", '2015-01-26', "action", 0, 10010, 20017), 
+            #Platform: ps4, xbox1, ps3, xbox360, msw
+            ("Battlefield 4", '2013-10-29', "shooter", 0, 10004, 20006),
+            #Platform: ps4, xbox1, msw
+            ("Call of Duty: Modern Warfare Remastered", '2016-11-04', "shooter", 0, 10012, 20018),
+            #Platform: ps4, xbox1, msw
+            ("Tom Clancy's Ghost Recon", '2017-03-7', "tactical shooter", 0, 10013, 20021),
+            #Platform: ps4 
+            ("Killzone Shadow Fall", '2013-11-15', "shooter", 1, 10005, 20024),
+            #Platform: ps4
+            ("The Last of Us Remastered", '2014-07-29', "survival-horror", 1, 10005, 20025)
+            #Platform: ps4, msw, xbox1
+            ("Star Wars: Battlefront", '2017-11-17', "shooter", 0, 10004, 20006)
+
+
+        ]
+        sql = "INSERT INTO Games VALUES(?, ?, ?, ?, ?, ?)"
+        _conn.executemany(sql, games)
+
+        _conn.commit()
+        print("success")
+
+    except Error as e:
+        _conn.rollback()
+        print(e)
+
+    print("++++++++++++++++++++++++++++++++++")
+
+def populateTable_DevPub(_conn):
+    print("++++++++++++++++++++++++++++++++++")
+    print("Populate Developer and Publisher tables:")
+
+    try: 
+        pub = [
+            ("Square Enix", 10001),
+            ("Feral Interactive", 10002),
+            ("Xbox Game Studios", 10003),
+            ("Electronic Arts", 10004),
+            ("Sony Interactive Entertainment", 10005),
+            ("505 Games", 10006), 
+            ("Blizzard Entertainment", 10007), 
+            ("2K Games", 10008),
+            ("Take-Two Interactive", 10009),
+            ("Techland", 10010),
+            ("Warner Bros. Interactive Entertainment", 10011),
+            ("Activision", 10012),
+            ("Ubisoft", 10013),
+            ("Gameloft", 10014),
+            ("Aspyr", 10015),
+            ("Frontier Groove, Inc.", 10016)
+
+        ]
+        sql = "INSERT INTO Publisher Values(?, ?)"
+        _conn.executemany(sql, pub)
+
+        dev = [
+            ("Crystal Dynamics", 20001),
+            ("Eidos-Montreal", 20002),
+            ("Feral Interactive", 20003),
+            ("Cameron Suey", 20004),
+            ("Respawn Entertainment", 20005),
+            ("DICE", 20006),
+            ("Motive Studios", 20007),
+            ("Criterion Software", 20008),
+            ("Kojima Productions", 20009),
+            ("Blizzard Entertainment", 20010),
+            ("Iron Galaxy", 20011),
+            ("Irrational Games", 20012),
+            ("2K Marin", 20013),
+            ("2K Australia", 20014),
+            ("Blind Squirrel Games", 20015),
+            ("Digital Extremes", 20016),
+            ("Techland", 20017),
+            ("Infinity Ward", 20018),
+            ("Raven Software", 20019),
+            ("Beenox", 20020),
+            ("Ubisoft", 20021),
+            ("Ubisoft Paris", 20022),
+            ("Red Storm Entertainment", 20023),
+            ("Guerrilla Games", 20024),
+            ("Naughty Dog", 20025)
+
+        ]
+        sql = "INSERT INTO Developer Values(?, ?)"
+        _conn.executemany(sql, dev)
+
+        _conn.commit()
+        print("success")
+
+    except Error as e:
+        _conn.rollback()
+        print(e)
     
+    print("++++++++++++++++++++++++++++++++++")
+    
+
+    
+def main():
+    database = r"data.sqlite"
+    conn = openConnection(database)
+    with conn:
+        createTables(conn)
+        populateTable_Games(conn)
+        #dropTables(conn)
+
+    closeConnection(conn, database)
+
+
+if __name__ == '__main__':
+    main()
